bb16a30c8c97fa1a616a87229777c46054473917
Gideon Opoku <opokugideon562@gmail.com>
2021-11-18 22:07:28 +0000

    classes file added

---
+++ classes.py
@@ -0,0 +1,29 @@
+class Point:
+    def __init__(self, x, y):  # constructor
+        self.x = x
+        self.y = y
+
+    def move(self):  # methods
+        print('move')
+
+    def draw(self):
+        print('draw')
+
+
+point = Point(1, 19)
+
+print(point.y)
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):  # methods
+        print(f'Hi, I am {self.name}')
+
+
+john = Person("Gideon")
+john.talk()
+
+bob = Person("Father Burgess")
+bob.talk()
