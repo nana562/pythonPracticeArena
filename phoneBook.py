0b0fad0a1cf4cdbd9d8c6a6ec90fa065394f679c
Gideon Opoku <opokugideon562@gmail.com>
2021-11-25 00:05:27 +0000

    phoneBook code added

---
+++ phoneBook.py
@@ -0,0 +1,17 @@
+phone = input('Phone: ')
+digits_mapping = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+    "0": "zero"
+}
+output = ""
+for ch in phone:
+    output += digits_mapping.get(ch, "!") + " "
+print(output)
